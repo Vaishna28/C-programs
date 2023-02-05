33e55f4a5d3b65a909caddeab63d375b17d165a3
Vaishnavi More <91751460+Vaishna28@users.noreply.github.com>
2023-02-05 18:35:06 +0530

    Update SumofElementsInArray.cpp

--- SumofElementsInArray.cpp
+++ SumofElementsInArray.cpp
@@ -1,1 +1,23 @@
-
+#include<iostream.h>
+#include<conio.h>
+int Sum(int arr[],int n)
+{
+  int sum1=0;
+  for(int i=0;i<n;i++)
+  {
+    sum+=arr[i];
+  }
+  return sum;
+}
+int main()
+{
+  int size;
+  cin>>size;
+  int arr[100];
+  for(int i=0;i<size;i++)
+  {
+    cin>>arr[i];
+  }
+  
+  cout<<"Sum of elements of array is:"<<sum(arr,n);
+}
